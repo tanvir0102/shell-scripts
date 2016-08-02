e7d18ba264aa44a72f21cd45eea02ab9108908ff
tanvir0102 <tanvir0102@gmail.com>
2016-08-02 17:49:36 +0530

    scrip to read input and display your information

---
+++ third_demo_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hey what's your first name? ";
+read fname;
+echo "Welcome Mr./Mrs. $fname, would you like to tell us, Your last name.";
+read lname;
+echo "Thanks Mr./Mrs. $fname $lname for telling us your name.";
+echo "******************************************************"
+echo "Mr./Mrs $lname, it's time to say you good bye."
